cddd96351e1f5d35a8c99ff1a2d74603e6a8550b
Sergey Kirpik <sergeykirpik2@gmail.com>
2017-09-28 19:26:28 +0300

    Field - invalid Point tests added

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,10 +1,17 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exception.AlreadyOccupiedException;
+import io.hexlet.xo.model.exception.InvalidPointException;
+
 import java.awt.*;
 
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+
+    private static final int MIN_COORDINATE = 0;
+
+    private static final int MAX_COORDINATE = FIELD_SIZE - 1;
 
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -12,19 +19,36 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(int x, int y) {
-        return field[x][y];
+    public Figure getFigure(int x, int y) throws InvalidPointException {
+        return getFigure(new Point(x, y));
     }
 
-    public Figure getFigure(Point point) {
-        return getFigure(point.x, point.y);
+    public Figure getFigure(Point point) throws InvalidPointException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+        return field[point.x][point.y];
     }
 
-    public void setFigure(int x, int y, Figure figure) {
-        field[x][y] = figure;
+    public void setFigure(int x, int y, Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+        setFigure(new Point(x, y), figure);
     }
 
-    public void setFigure(Point point, Figure figure) {
-        setFigure(point.x, point.y, figure);
+    public void setFigure(Point point, Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+        if (getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+        field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate <= MAX_COORDINATE;
     }
 }
