beec4a7064aa2422c2473799c1c51731f7fef256
Sergey Kirpik <sergeykirpik@gmail.com>
2017-10-03 01:22:27 +0300

    WinnerController::getWinner - finished v.1

--- src/io/hexlet/xo/controller/WinnerController.java
+++ src/io/hexlet/xo/controller/WinnerController.java
@@ -12,22 +12,33 @@ public class WinnerController {
 
         try {
             Figure f1, f2, f3;
+            int x, y;
+
+            for (x = 0; x < field.getSize(); x++) {
+                f1 = field.getFigure(new Point(x, 0));
+                f2 = field.getFigure(new Point(x, 1));
+                f3 = field.getFigure(new Point(x, 2));
+
+                if (f1 == f2 && f1 == f3) return f1;
+            }
+
+            for (y = 0; y < field.getSize(); y++) {
+                f1 = field.getFigure(new Point(0, y));
+                f2 = field.getFigure(new Point(1, y));
+                f3 = field.getFigure(new Point(2, y));
+
+                if (f1 == f2 && f1 == f3) return f1;
+            }
 
             f1 = field.getFigure(new Point(0, 0));
-            f2 = field.getFigure(new Point(0, 1));
-            f3 = field.getFigure(new Point(0, 2));
-
-            if (f1 == f2 && f1 == f3) return f1;
-
-            f1 = field.getFigure(new Point(1, 0));
             f2 = field.getFigure(new Point(1, 1));
-            f3 = field.getFigure(new Point(1, 2));
+            f3 = field.getFigure(new Point(2, 2));
 
             if (f1 == f2 && f1 == f3) return f1;
 
-            f1 = field.getFigure(new Point(2, 0));
-            f2 = field.getFigure(new Point(2, 1));
-            f3 = field.getFigure(new Point(2, 2));
+            f1 = field.getFigure(new Point(0, 2));
+            f2 = field.getFigure(new Point(1, 1));
+            f3 = field.getFigure(new Point(2, 0));
 
             if (f1 == f2 && f1 == f3) return f1;
 
