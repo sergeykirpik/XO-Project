7f9255ea86fec27a09a970286ea96cac167e3d0f
Sergey Kirpik <sergeykirpik@gmail.com>
2017-10-03 01:13:41 +0300

    WinnerController::getWinner

--- src/io/hexlet/xo/controller/WinnerController.java
+++ src/io/hexlet/xo/controller/WinnerController.java
@@ -2,7 +2,39 @@ package io.hexlet.xo.controller;
 
 import io.hexlet.xo.model.Field;
 import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exception.InvalidPointException;
+
+import java.awt.*;
 
 public class WinnerController {
 
+    public Figure getWinner(final Field field) {
+
+        try {
+            Figure f1, f2, f3;
+
+            f1 = field.getFigure(new Point(0, 0));
+            f2 = field.getFigure(new Point(0, 1));
+            f3 = field.getFigure(new Point(0, 2));
+
+            if (f1 == f2 && f1 == f3) return f1;
+
+            f1 = field.getFigure(new Point(1, 0));
+            f2 = field.getFigure(new Point(1, 1));
+            f3 = field.getFigure(new Point(1, 2));
+
+            if (f1 == f2 && f1 == f3) return f1;
+
+            f1 = field.getFigure(new Point(2, 0));
+            f2 = field.getFigure(new Point(2, 1));
+            f3 = field.getFigure(new Point(2, 2));
+
+            if (f1 == f2 && f1 == f3) return f1;
+
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
